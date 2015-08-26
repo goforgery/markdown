e58c349bd67b7ffd47ee9f8f518b59891adc8b85
Ric Allinson <capecodehq@yahoo.co.uk>
2015-08-26 14:02:55 -0700

    Update to readme

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Simple markdown renderer and caching package for Forgery2 based on [blackfriday]
 
 * `.Render()` Function that renders the given filename as markdown.
 * `.Clean()` Function that empties the cache of any rendered files.
+
+## Use
+
+    package main
+
+    import(
+        "fmt"
+        "github.com/goforgery/markdown"
+    )
+
+    func init() {
+        html := markdown.Render("./text.md")
+        fmt.Println(html)
+        markdown.Clean()
+    }
--- renderer.go
+++ renderer.go
@@ -1,40 +1,36 @@
 package markdown
 
-import(
-    "io/ioutil"
-    "github.com/russross/blackfriday"
+import (
+	"github.com/russross/blackfriday"
+	"io/ioutil"
 )
 
 type Markdown struct {
-    cache map[string]string
+	cache map[string]string
 }
 
 var this *Markdown
 
 // Render the given file as markdown.
-func Render(f string) (string) {
-
-    if this == nil {
-        this = &Markdown{}
-        Clean()
-    }
-
-    c, ok := this.cache[f]
-
-    if ok == false {
-        d, err := ioutil.ReadFile(f)
-        if err != nil {
-            return ""
-        }
-        s := blackfriday.MarkdownBasic(d)
-        this.cache[f] = string(s)
-        c = this.cache[f]
-    }
-
-    return c
+func Render(f string) string {
+	if this == nil {
+		this = &Markdown{}
+		Clean()
+	}
+	c, ok := this.cache[f]
+	if ok == false {
+		d, err := ioutil.ReadFile(f)
+		if err != nil {
+			return ""
+		}
+		s := blackfriday.MarkdownBasic(d)
+		this.cache[f] = string(s)
+		c = this.cache[f]
+	}
+	return c
 }
 
 // Clean the cache.
 func Clean() {
-    this.cache = map[string]string{}
+	this.cache = map[string]string{}
 }
